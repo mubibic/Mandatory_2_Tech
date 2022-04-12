492d9eecf2d6797c7cd6891bc6716faa3ebd5fc9
BalazsGitHub <balazs.dombovari@gmail.com>
2022-04-12 12:39:18 +0200

    Added some comments and refractored some variables

--- Ball.java
+++ Ball.java
@@ -3,32 +3,34 @@ import java.util.Random;
 import javax.swing.*;
 
 
-class Ball extends JPanel implements Runnable
-{
-    private boolean xUp, yUp, bouncing;
-    private int x, y, xDx, yDy;
-    private final int MAX_X = 400, MAX_Y = 400;
+class Ball extends JPanel implements Runnable {
+    //Direction of x and y coordinates
+    private boolean xIncrease, yIncrease;
+    //Top left coordinate point of the ball
+    private int x, y;
+    //The amount/speed we increase the coordinates
+    private int xDelta, yDelta;
+    //Size of the frame
+    private final int MAX_X = 400, MAX_Y = 360;
+    //Random variable for color change when colliding
     private Random r;
     private Color clr = Color.blue;
 
 
-    public Ball()
-    {
-        xUp = false;
-        yUp = false;
-        xDx = 1;
-        yDy = 1;
-        bouncing = true;
-
+    public Ball() {
+        xIncrease = false;
+        yIncrease = false;
+        xDelta = 1;
+        yDelta = 1;
 
 
         setOpaque(false);
-        setPreferredSize(new Dimension( MAX_X, MAX_Y));
+        setPreferredSize(new Dimension(MAX_X, MAX_Y));
         r = new Random();
     }
-    public void run()
-    {
-        while ( true ) {
+
+    public void run() {
+        while (true) {
 
             try {
                 Thread.sleep(10);
@@ -36,46 +38,41 @@ class Ball extends JPanel implements Runnable
                 System.err.println(exception.toString());
             }
 
-            if (xUp) {
-                x += xDx;
+            if (xIncrease) {
+                x += xDelta;
             } else {
-                x -= xDx;
+                x -= xDelta;
             }
 
 
-            if (yUp){
-                y += yDy;
-            }
-            else{
-                y -= yDy;
-            }
-
-
-            if ( y <= 0 ) {
-                yUp = true;
-                yDy = ( int ) ( Math.random() * 5 + 2 );
-                set(new Color(r.nextInt(256), r.nextInt(256),r.nextInt(256)));
-
+            if (yIncrease) {
+                y += yDelta;
+            } else {
+                y -= yDelta;
             }
 
-            else if ( y >= MAX_Y - 30 ) {
-                yDy = ( int ) ( Math.random() * 5 + 2 );
-                yUp = false;
-                set(new Color(r.nextInt(256), r.nextInt(256),r.nextInt(256)));
+
+            if (y <= 0) {
+                yIncrease = true;
+                yDelta = (int) (Math.random() * 5 + 2);
+                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+
+            } else if (y >= MAX_Y - 30) {
+                yDelta = (int) (Math.random() * 5 + 2);
+                yIncrease = false;
+                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
 
             }
 
-            if ( x <= 0 ) {
-                xUp = true;
-                xDx = ( int ) ( Math.random() * 5 + 2 );
-                set(new Color(r.nextInt(256), r.nextInt(256),r.nextInt(256)));
+            if (x <= 0) {
+                xIncrease = true;
+                xDelta = (int) (Math.random() * 5 + 2);
+                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
 
-            }
-
-            else if ( x >= MAX_X - 30 ) {
-                xUp = false;
-                xDx = ( int ) ( Math.random() * 5 + 2 );
-                set(new Color(r.nextInt(256), r.nextInt(256),r.nextInt(256)));
+            } else if (x >= MAX_X - 30) {
+                xIncrease = false;
+                xDelta = (int) (Math.random() * 5 + 2);
+                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
 
             }
 
@@ -86,20 +83,17 @@ class Ball extends JPanel implements Runnable
     }
 
 
-    public void paint(Graphics gr)
-    {
+    public void paint(Graphics gr) {
 
         super.paint(gr);
-        if ( bouncing ) {
 
-            gr.setColor(get());
-            gr.fillOval( x, y, 30, 30 );
-        }
+        gr.setColor(get());
+        gr.fillOval(x, y, 30, 30);
 
     }
 
-    public void set(Color c){
-        clr=c;
+    public void set(Color c) {
+        clr = c;
     }
 
     public Color get() {
