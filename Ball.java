40400bba22f5166e6cf566a893a1e225872819b5
BalazsGitHub <balazs.dombovari@gmail.com>
2022-04-12 15:39:32 +0200

    Rewritten version of bouncing and colliding balls, using swing as graphical display

--- Ball.java
+++ Ball.java
@@ -1,8 +1,7 @@
 import java.awt.*;
 import java.util.Random;
-import javax.swing.*;
 
-
+//Runnable is needed to run the balls on multiple threads
 class Ball implements Runnable {
     //Direction of x and y coordinates
     private boolean xIncrease, yIncrease;
@@ -16,67 +15,94 @@ class Ball implements Runnable {
     private Random r;
     public Color clr = Color.blue;
 
-
-    public Ball() {
+    //Constructor requesting starting coordinates
+    public Ball(int x, int y) {
+        //Boolean if we increase or decrease the x/y coordinate value of the ball
         xIncrease = false;
         yIncrease = false;
-        xDelta = 1;
-        yDelta = 1;
-
-
+        //Delta refers to the balls speed
+        xDelta = 5;
+        yDelta = 5;
+        this.x = x;
+        this.y = y;
+        //Instantiating variable for random color
         r = new Random();
     }
 
+    //This method is called when the thread starts (thread.start method calls it); Run(method is a must because of using Runnable interface
     public void run() {
         while (true) {
-
             try {
-                Thread.sleep(10);
+                Thread.sleep(20);
             } catch (InterruptedException exception) {
                 System.err.println(exception.toString());
             }
+            //Continuously calculate the new coordinates based on Delta(speed) values
+            moveBallOneStep();
 
-            if (xIncrease) {
-                x += xDelta;
-            } else {
-                x -= xDelta;
-            }
-
-
-            if (yIncrease) {
-                y += yDelta;
-            } else {
-                y -= yDelta;
-            }
-
-
+            //Checking if ball colliding with the wall/so it can not leave the frame
             if (y <= 0) {
                 yIncrease = true;
-                yDelta = (int) (Math.random() * 5 + 2);
-                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+                //It is the randomizer for the speed of the ball after colliding
+                //yDelta = (int) (Math.random() * 5 + 2);
+                setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
 
-            } else if (y >= MAX_Y - 30) {
-                yDelta = (int) (Math.random() * 5 + 2);
+                //+30 is the diameter of the ball as we don't use it's center but the top left point of it
+            } else if (y + 30 >= MAX_Y) {
+                //yDelta = (int) (Math.random() * 5 + 2);
                 yIncrease = false;
-                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+                setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
 
             }
 
             if (x <= 0) {
                 xIncrease = true;
-                xDelta = (int) (Math.random() * 5 + 2);
-                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+                //xDelta = (int) (Math.random() * 5 + 2);
+                setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
 
-            } else if (x >= MAX_X - 30) {
+            } else if (x + 30 >= MAX_X) {
                 xIncrease = false;
-                xDelta = (int) (Math.random() * 5 + 2);
-                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+                //xDelta = (int) (Math.random() * 5 + 2);
+                setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
 
             }
         }
     }
 
-    public void set(Color c) {
+    //Checks if the balls centers(diameter of the ball is 30) collide with each other
+    public boolean isHitboxHit(Ball ball) {
+        //Returns true if the two balls center points are closer than 30 -> they collided
+        return (calculateDistanceBetweenPoints(this.x + 15, this.y + 15, ball.x + 15, ball.y + 15) < 30);
+    }
+
+    //Mathematical calculation for the distance btw the balls coordinates for collision detection
+    private double calculateDistanceBetweenPoints(double x1, double y1, double x2, double y2) {
+        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+    }
+
+    //Change balls direction after collision
+    public void invertDirection() {
+        xIncrease = !xIncrease;
+        yIncrease = !yIncrease;
+        moveBallOneStep();
+    }
+
+    //Calculate the new coordinates based on Delta(speed) values
+    private void moveBallOneStep() {
+        if (xIncrease) {
+            x += xDelta;
+        } else {
+            x -= xDelta;
+        }
+
+        if (yIncrease) {
+            y += yDelta;
+        } else {
+            y -= yDelta;
+        }
+    }
+
+    public void setColor(Color c) {
         clr = c;
     }
 
--- BouncingBalls.java
+++ BouncingBalls.java
@@ -1,52 +1,71 @@
 import javax.swing.*;
 import java.awt.*;
-import java.lang.reflect.Array;
-import java.util.Random;
 
-class BouncingBalls extends JFrame {
+class BouncingBalls extends Canvas {
 
-    private Ball balls[] = new Ball[3];
-
+    private Ball balls[] = new Ball[4];
 
     public BouncingBalls() {
 
-        Random r = new Random();
-        setResizable(false);
-        setSize(400, 400);
+        Ball ball1 = new Ball(0, 0);
+        Ball ball2 = new Ball(100,200);
+        Ball ball3 = new Ball(150, 50);
+        Ball ball4 = new Ball(50, 150);
 
-        Ball ball1 = new Ball();
-        Ball ball2 = new Ball();
-        Ball ball3 = new Ball();
-
-
-        //getContentPane().add(ball1);
-        setVisible(true);
+        //Creating an own thread for each ball -> built in method from Runnable interface
         Thread x = new Thread(ball1);
         Thread y = new Thread(ball2);
         Thread z = new Thread(ball3);
+        Thread v = new Thread(ball4);
 
+        //Start() method calls Run()method
         x.start();
         y.start();
         z.start();
+        v.start();
 
         balls[0] = ball1;
         balls[1] = ball2;
         balls[2] = ball3;
-
+        balls[3] = ball4;
     }
 
     public static void main(String[] args) {
-        new BouncingBalls();
+        //Calling the constructor of start()method and creates the threads
+        BouncingBalls bb = new BouncingBalls();
+        //Jframe is for graphical display/ This creating the window
+        JFrame f = new JFrame();
+        f.add(bb);
+        f.setResizable(false);
+        f.setSize(400, 400);
+        f.setVisible(true);
+
+        //Refreshing loop for graphical display
+        while (true) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException exception) {
+                System.err.println(exception.toString());
+            }
+            //The logic for checking if the balls are colliding / Comparing all balls with all other balls
+            for (int i = 0; i < bb.balls.length; i++) {
+                for (int j = i + 1; j < bb.balls.length; j++) {
+                    if (bb.balls[i].isHitboxHit(bb.balls[j])) {
+                        bb.balls[i].invertDirection();
+                        bb.balls[j].invertDirection();
+                    }
+                }
+            }
+            // It is called every 20 millisecs which calls paint method in the background
+            bb.repaint();
+        }
     }
 
+    //This method draws the balls themselves
     public void paint(Graphics gr) {
-
-        super.paint(gr);
-
         for (Ball ball : balls) {
             gr.setColor(ball.getColor());
             gr.fillOval(ball.x, ball.y, 30, 30);
         }
-
     }
 }
