66ed33fbfd4ad4578402e9bfa63142e566698520
Dani <you@example.com>
2022-04-12 14:35:41 +0200

    -Collision works more or less

--- src/Ball.java
+++ src/Ball.java
@@ -1,22 +1,35 @@
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import javax.swing.*;
 
 class Ball extends JPanel implements Runnable {
-    private boolean xUp, yUp, bouncing;
-    private int x, y;
+
+    private int id;
+    private ArrayList<Position> positionList;
+
+    private boolean xUp, yUp;
+
+    //current position of ball
+    // private int x, y;
+
+    //new direction and speed after bounce
     private int xDx, yDy;
+    //Bouncing frame
     private final int MAX_X = 400, MAX_Y = 360;
+
     private Random r;
     private Color clr = Color.blue;
 
-    public Ball() {
+    public Ball(int id, ArrayList<Position> positionList) {
+        this.id = id;
+        this.positionList = positionList;
 
         xUp = false;
         yUp = false;
         xDx = 1;
         yDy = 1;
-        bouncing = true;
 
         setOpaque(false);
         setPreferredSize(new Dimension(MAX_X, MAX_Y));
@@ -26,58 +39,105 @@ class Ball extends JPanel implements Runnable {
     public void run() {
 
         while (true) {
-
-            try {
-                Thread.sleep(15);
-            } catch (InterruptedException exception) {
-                System.err.println(exception.toString());
-            }
-
-            if (xUp) {
-                x += xDx;
-            } else {
-                x -= xDx;
-            }
-
-            if (yUp) {
-                y += yDy;
-            } else {
-                y -= yDy;
-            }
-
-            if (y <= 0) {
-                yUp = true;
-                yDy = (int) (Math.random() * 5 + 2);
-                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
-
-            } else if (y >= MAX_Y - 30) {
-                yDy = (int) (Math.random() * 5 + 2);
-                yUp = false;
-                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
-            }
-
-            if (x <= 0) {
-                xUp = true;
-                xDx = (int) (Math.random() * 5 + 2);
-                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
-
-            } else if (x >= MAX_X - 30) {
-                xUp = false;
-                xDx = (int) (Math.random() * 5 + 2);
-                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
-            }
-
+            setFps(15);
+            Position position = positionList.get(id);
+            moveBall(position);
+            ballCollision(position);
+            wallCollision(position);
             repaint();
         }
     }
 
+    private void setFps(int milliSecond) {
+        try {
+            Thread.sleep(milliSecond);
+        } catch (InterruptedException exception) {
+            System.err.println(exception.toString());
+        }
+    }
+
+    private void wallCollision(Position position) {
+        //If bottom frame reached, direction set to up
+        if (position.getY() <= 0) {
+            yUp = true;
+            yDy = (int) (Math.random() * 5 + 2);
+
+
+        } else if (position.getY() >= MAX_Y - 30) {
+            yDy = (int) (Math.random() * 5 + 2);
+            yUp = false;
+
+        }
+
+        if (position.getX() <= 0) {
+            xUp = true;
+            xDx = (int) (Math.random() * 5 + 2);
+
+
+        } else if (position.getX() >= MAX_X - 30) {
+            xUp = false;
+            xDx = (int) (Math.random() * 5 + 2);
+        }
+    }
+
+    private void moveBall(Position position) {
+        int x = position.getX();
+        int y = position.getY();
+        //New random (xDx) generated
+        if (xUp) {
+            //Set x to x+=xdx
+            position.setX(x + xDx);
+        } else {
+            position.setX(x - xDx);
+        }
+
+        if (yUp) {
+            position.setY(y + yDy);
+        } else {
+            position.setY(y - yDy);
+        }
+    }
+
+    public void ballCollision(Position position) {
+        //Copy list
+        List<Position> otherPositions = new ArrayList<>(positionList);
+        otherPositions.remove(id);
+
+        for (Position otherPosition : otherPositions
+        ) {
+            double xDif = position.getX() - otherPosition.getX();
+            double yDif = position.getY() - otherPosition.getY();
+            double distanceSquared = xDif * xDif + yDif * yDif;
+
+            boolean collision = distanceSquared < (15 + 15) * (15 + 15);
+
+            if (collision) {
+                yDy = (int) (Math.random() * 5 + 2);
+                xDx = (int) (Math.random() * 5 + 2);
+                set(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+
+                if (!yUp) {
+                    yUp = true;
+                } else {
+                    yUp = false;
+                }
+
+                if (!xUp) {
+                    xUp = true;
+                } else {
+                    xUp = false;
+                }
+            }
+        }
+    }
+
     public void paint(Graphics gr) {
+        int x = positionList.get(id).getX();
+        int y = positionList.get(id).getY();
 
         super.paint(gr);
-        if (bouncing) {
-            gr.setColor(get());
-            gr.fillOval(x, y, 30, 30);
-        }
+        gr.setColor(get());
+        gr.fillOval(x, y, 30, 30);
     }
 
     public void set(Color c) {
--- src/BouncingBalls.java
+++ src/BouncingBalls.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 class BouncingBalls extends JFrame {
@@ -10,13 +10,24 @@ class BouncingBalls extends JFrame {
         setResizable(false);
         setSize(400, 400);
 
-        Ball ball1 = new Ball();
-        Ball ball2 = new Ball();
-        Ball ball3 = new Ball();
+        //Id not needed, ArrayList index = ball id
+        Position ballPosition1 = new Position(1,1);
+        Position ballPosition2 = new Position(1,1);
+        Position ballPosition3 = new Position(1,1);
+
+        ArrayList<Position> positionList = new ArrayList<>();
+        positionList.add(ballPosition1);
+        positionList.add(ballPosition2);
+        positionList.add(ballPosition3);
+
+        Ball ball1 = new Ball(0,positionList);
+        Ball ball2 = new Ball(1,positionList);
+        Ball ball3 = new Ball(2,positionList);
 
         ball1.add(ball2);
         ball2.add(ball3);
         getContentPane().add(ball1);
+
         setVisible(true);
         Thread x = new Thread(ball1);
         Thread y = new Thread(ball2);
